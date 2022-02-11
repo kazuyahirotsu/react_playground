42ddbdee05c6a0cbe72acaa844de6c2fee784406
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-02-11 23:07:31 +0900

    Learned various ways to write CSS

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import { useEffect } from "react";
 //propsでcolor、childrenでmessageを指定して他のファイルの関数でhtmlを作る
 import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
+import { TailwindCss } from "./components/TailwindCss";
 
 export const App = () => {
   console.log("rendering");
@@ -25,6 +30,10 @@ export const App = () => {
       <ColoredMessage color="pink" > Great!! This is children </ColoredMessage>
       <button onClick={onClickButton}>button</button>
       <p>{num}</p>
+
+      <CssModules></CssModules>
+      <StyledJsx></StyledJsx>
+      <TailwindCss></TailwindCss>
     </>
   );
 };
---
+++ my-app/src/components/TailwindCss.jsx
@@ -0,0 +1,10 @@
+//not working
+
+export const TailwindCss = () => {
+    return (
+        <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
+            <p className="m-0 text-gray-400">This is Tailwind CSS</p>
+            <button>button</button>
+        </div>
+    )
+}
