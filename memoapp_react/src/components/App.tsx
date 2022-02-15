3697899d23a6290a756c170d3f28633a415f6e04
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-02-16 00:14:28 +0900

    Made a simple memo app with react

---
+++ memoapp_react/src/components/App.tsx
@@ -0,0 +1,32 @@
+import {ChangeEvent, useState, FC, useCallback} from "react";
+import styled from "styled-components";
+import {MemoList} from "./MemoList";
+import {useMemoList} from "../hooks/useMemoList"
+
+export const App: FC = () => {
+  const {memos, addTodo, deleteTodo} = useMemoList();
+  const [text, setText] = useState<string>("");
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+
+  const onClickAdd = () => {
+    addTodo(text);
+    setText("");
+  };
+
+  const onClickDelete = useCallback((index: number) => {
+    deleteTodo(index);
+  }, [deleteTodo]);
+
+  return (
+    <div>
+      <h1>simple memo app</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>add</SButton>
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
+    </div>
+  );
+};
+const SButton = styled.button`
+  margin-left: 16px;
+`;
