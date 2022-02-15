926581c766880421ea04af81a2559fe7d729bd48
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-02-15 23:02:09 +0900

    Practiced TypeScript

--- ts_practice.ts
+++ ts_practice.ts
@@ -19,8 +19,51 @@ null1 = null;
 let undefined1: undefined = undefined;
 undefined1 = undefined;
 
-let any1: any = any;
+let any1: any;
 any1 = false;
 any1 = 10;
 any1 = undefined;
+
+const funcA = (num: number): void => {
+    console.log(num);
+};
+funcA(10);
+
+const obj: {str:string, num:number} = {
+    str: "A",
+    num: 10,
+};
+obj.str = "B"
+obj.num = 20
+
+const obj2: { str:string } & { num:number } = {
+    str: "A",
+    num: 10,
+};
+obj2.str = "20"
+
+type TypeA = {
+    str: string;
+    num: number;
+}
+type TypeB = {
+    str: string;
+    bool: boolean;
+}
+type TypeC = TypeA & TypeB;
+
+const obj1: TypeC = {
+    str: "A",
+    num: 10,
+    bool: false,
+};
+
+let val1: string | number = "";
+val1 = "A";
+val1 = 10;
+
+type CustomType<T> = {
+    val: T;
+}
+const strObj: CustomType<string> = {val: "A"};
 
